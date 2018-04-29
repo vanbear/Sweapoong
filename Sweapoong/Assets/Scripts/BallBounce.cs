21b221c4605bd39dcf76b5b83581780348d66957
Lyszard <lyszardkrol@gmail.com>
2018-04-29 16:45:30 +0200

    Prototype
    Ball physics, touch controls, Walls

---
+++ Sweapoong/Assets/Scripts/BallBounce.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour 
+{
+
+
+	public Rigidbody2D rb;
+	public BoxCollider2D boxCollider;
+	public CircleCollider2D circleCollider;
+	public float force;
+	Vector2 startPos;
+	Vector2 endPos;
+	Vector2 direction;
+	float touchTimeStart;
+	float touchTimeEnd;
+	float touchTimeInterval;
+	bool isTouched = false;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb.AddForce (new Vector2 (force, force));
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) 
+		{
+			
+
+			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+			if (boxCollider.OverlapPoint(wp))
+			{
+				//your code
+				rb.velocity = Vector2.zero;
+				Debug.Log ("Hello");
+				isTouched = true;
+				touchTimeStart = Time.time;
+				startPos = Input.GetTouch (0).position;
+			}
+
+
+		}
+
+
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Ended && isTouched == true) 
+		{
+
+			touchTimeEnd = Time.time;
+			touchTimeInterval = touchTimeEnd - touchTimeStart;
+			endPos = Input.GetTouch (0).position;
+			direction = startPos - endPos;
+
+			GetComponent<Rigidbody2D> ().AddForce (-direction/touchTimeInterval * force,ForceMode2D.Impulse);
+			isTouched = false;
+
+		}
+
+		
+	}
+}
---
+++ Sweapoong/Assets/Scripts/PlayerWallCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallCollision : MonoBehaviour 
+{
+	
+	public int player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Ball")
+		{
+			Debug.Log ("Player" +player.ToString()+ "wall was hit");
+
+		}
+	}
+}
