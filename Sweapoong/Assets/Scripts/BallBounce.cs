db5983a440fef8c431f98bbff70c841f9181f9b1
vanbear <dbearb@gmail.com>
2018-05-20 18:45:21 +0200

    Changed background and ball speed behavior

--- Sweapoong/Assets/Scripts/BallBounce.cs
+++ Sweapoong/Assets/Scripts/BallBounce.cs
@@ -5,25 +5,33 @@ using UnityEngine;
 public class BallBounce : MonoBehaviour 
 {
 
-
+	// MODULES
 	public Rigidbody2D rb;
 	public BoxCollider2D boxCollider;
 	public CircleCollider2D circleCollider;
+
+	// VALUES
 	public float force;
+
 	Vector2 startPos;
 	Vector2 endPos;
 	Vector2 direction;
+
 	float touchTimeStart;
 	float touchTimeEnd;
 	float touchTimeInterval;
 	bool isTouched = false;
+
+	float startSpeed = 300.0f;
+	public float speedMultiplier = 3.0f;
+	float currentSpeed;
 
 
 	// Use this for initialization
 	void Start () 
 	{
 		rb.AddForce (new Vector2 (force, force));
-		
+		currentSpeed = startSpeed;
 	}
 	
 	// Update is called once per frame
@@ -31,20 +39,16 @@ public class BallBounce : MonoBehaviour
 	{
 		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) 
 		{
-			
-
 			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 			if (boxCollider.OverlapPoint(wp))
 			{
 				//your code
-				rb.velocity = Vector2.zero;
-				Debug.Log ("Hello");
+				rb.velocity = Vector2.zero; // stop ball when touched
+				Debug.Log ("Ball touched.");
 				isTouched = true;
 				touchTimeStart = Time.time;
 				startPos = Input.GetTouch (0).position;
 			}
-
-
 		}
 
 
@@ -53,10 +57,21 @@ public class BallBounce : MonoBehaviour
 
 			touchTimeEnd = Time.time;
 			touchTimeInterval = touchTimeEnd - touchTimeStart;
+			// get touch position
 			endPos = Input.GetTouch (0).position;
-			direction = startPos - endPos;
 
-			GetComponent<Rigidbody2D> ().AddForce (-direction/touchTimeInterval * force,ForceMode2D.Impulse);
+			// get swipe direction and normalize
+			direction = startPos - endPos;
+			direction = direction.normalized;
+
+			// add speed
+			currentSpeed += speedMultiplier*(Mathf.Log(currentSpeed,2));
+			Debug.Log ("Current speed: "+currentSpeed);
+
+			// add force to ball
+			GetComponent<Rigidbody2D> ().AddForce (-direction * currentSpeed, ForceMode2D.Impulse);
+
+
 			isTouched = false;
 
 		}
