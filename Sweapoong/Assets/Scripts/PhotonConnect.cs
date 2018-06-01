6ad54eed239149f921c6cd5fd7f7afcff990e433
Lyszard <lyszardkrol@gmail.com>
2018-06-01 20:08:14 +0200

    Menu + Connection to Photon Servers
    -Added Start Menu
    -Added PhotonNetworkManager to handle all of the photon stuff

---
+++ Sweapoong/Assets/Scripts/PhotonConnect.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonConnect : Photon.PunBehaviour 
+{
+
+	public string versionName = "0.99";
+
+	public void ConnectToPhoton()
+	{
+		PhotonNetwork.ConnectUsingSettings (versionName);
+		Debug.Log ("Connecting to photon...");
+
+	}
+
+
+	public override void OnConnectedToMaster()
+	{
+		//PhotonNetwork.JoinLobby(TypedLobby.Default);
+		PhotonNetwork.JoinRandomRoom();
+		Debug.Log ("Connected to Master");
+
+	}
+
+	public override void OnJoinedLobby()
+	{
+		Debug.Log ("joined lobby");
+	}
+
+	public override void OnDisconnectedFromPhoton()
+	{
+		Debug.LogWarning("Disconnected from photon");        
+	}
+
+	public override void OnPhotonRandomJoinFailed (object[] codeAndMsg)
+	{
+		Debug.Log("No Rooms, Creating a new one");
+		// #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+		PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 2 }, null);
+	}
+
+	public override void OnJoinedRoom()
+	{
+		Debug.Log("Joined a room");
+	}
+}
