4f8f9b8aeed0ead9d38091acb86609a6b4ad121d
Lyszard <lyszardkrol@gmail.com>
2018-06-02 16:44:38 +0200

    Trying to load level
    Trying to load level when 2 players have joined the room

--- Sweapoong/Assets/Scripts/PhotonConnect.cs
+++ Sweapoong/Assets/Scripts/PhotonConnect.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class PhotonConnect : Photon.PunBehaviour 
 {
 
 	public string versionName = "0.99";
+	//public string playerName = Random.Range(1000,9999).ToString();
 
+	void Start()
+	{
+		PhotonNetwork.automaticallySyncScene = true;
+		PhotonNetwork.playerName = "Player" + Random.Range (1000, 9999).ToString();
+
+	}
 	public void ConnectToPhoton()
 	{
 		PhotonNetwork.ConnectUsingSettings (versionName);
+		//PhotonNetwork.playerName = playerName;
 		Debug.Log ("Connecting to photon...");
 
 	}
@@ -37,11 +45,25 @@ public class PhotonConnect : Photon.PunBehaviour
 	{
 		Debug.Log("No Rooms, Creating a new one");
 		// #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
-		PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 2 }, null);
+		PhotonNetwork.CreateRoom(null, new RoomOptions() { IsVisible = true, MaxPlayers = 2 }, null);
+
 	}
 
 	public override void OnJoinedRoom()
 	{
 		Debug.Log("Joined a room");
+		Debug.Log ("PLAYERNAME: " + PhotonNetwork.playerName);
 	}
+
+	public override void OnPhotonPlayerConnected(PhotonPlayer newPLayer)
+	{
+		Debug.Log ("Second PLayer has joined");
+		if (PhotonNetwork.countOfPlayers == 2) 
+		{
+			PhotonNetwork.LoadLevel ("GameScene");
+		}
+
+	}
+
+
 }
