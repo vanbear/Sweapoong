a5ce1caeaa6f5b84a417b9479a152cc5766e50d6
vanbear <dbearb@gmail.com>
2018-06-03 16:16:51 +0200

    Started working on Powerups

---
+++ Sweapoong/Assets/Scripts/Powerups/Shield.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour {
+	int possesion;
+	int health = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		// if hit by ball
+		if (col.gameObject.tag == "Ball")
+		{
+			// decrease health
+			health--;
+			// if health drops down to 0 -> destroy
+			if (health == 0)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ Sweapoong/Assets/Scripts/Powerups/ShieldPowerup.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPowerup : Powerup {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Shield p-up created");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		// check if collided with ball's circleCollider (we don't want to trigger it with ball's expanded box collider)
+		if (col.gameObject.tag == "Ball" && col.GetType() == typeof(CircleCollider2D))
+		{
+			// get current ball's possesion
+			int pos = col.gameObject.GetComponent<BallBounce>().possesion;
+			// check if the ball is actually in anybody's possesion
+			if (pos != 0)
+			{
+				/////////////////
+				// TODO:
+				// check if there is no shield already in given player's possesion
+				// if not:
+					// create shield for given player
+				// else:
+					// increase his shield's health up to 3
+				/////////////////
+				
+				
+
+				// destroy the pickup
+				Debug.Log(pos);
+				Destroy(gameObject);
+			}
+			// nothing happens when the ball is ownerless		
+		}
+	}
+}
